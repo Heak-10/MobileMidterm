325873fa68c42807f06bd86c2cf1bc291dab930c
Heak-10 <nayheakh@gmail.com>
2025-08-29 23:08:34 +0700

    Add files via upload

---
+++ lib/Helper/style.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+class Stylecolor {
+  static const Color primary = Color(0xFF8E24AA); // Purple
+  static const Color background = Color(0xFFFCE4EC); // Light pink
+  static const Color filledBackground = Color(0xFFD81B60); // Pink Accent
+  static const Color filledBackground2 = Color(0x22D81B60); // Transparent pink
+}
